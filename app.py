e99fa15595cfafd50c77ad9622b809ed1d11368f
Hanson Braganza <hansonnnnnnn@gmail.com>
2022-04-21 03:28:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, render_template
-
-
+import config
+import azure.cosmos.cosmos_client as cosmos_client
+import azure.cosmos.exceptions as exceptions
+import uuid
 app = Flask(__name__)
 
 
@@ -12,6 +14,23 @@ def home():
 @app.route('/shop')
 def shop():
     return render_template('shop.html')
+
+@app.route('/free', methods=["GET", "POST"])
+def free():
+    if request.method == "POST":
+        order = {
+            'id': "'"+str(uuid.uuid1())+"'",
+            'email': "hanson@gmail.com",
+            'name': "Hanson",
+            'contactNum': 1234567891
+        }
+        try:
+            container.create_item(body=order)
+            return render_template('shop.html', result="Use Coupon code: fer25324f32fg")
+
+        except exceptions.CosmosResourceExistsError:
+            return render_template('index.html', result="Already claimed")
+    return render_template('index.html')
 
 
 
